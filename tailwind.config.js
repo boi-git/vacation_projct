814b79f07cee63b8487b1b0e5ed5387941055f2a
boi-git <adhwa.aizat23@gmail.com>
2024-10-24 17:35:44 +0800

    Update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.html", // This will include all HTML files in the same folder
+  ],
   theme: {
     extend: {},
   },
